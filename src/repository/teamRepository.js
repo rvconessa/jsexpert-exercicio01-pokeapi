49eb45d0d3bdaf29d432f95a518a092212e72337
Renan <rvconessa@gmail.com>
2022-02-21 16:58:20 -0300

    feat: find selected pokemons

--- src/repository/teamRepository.js
+++ src/repository/teamRepository.js
@@ -6,7 +6,7 @@ class TeamRepository {
     }
     
     async getRandomPokemons() {
-       const data =  await  request("https://pokeapi.co/api/v2/pokemon?limit=150")
+       const data =  await request("https://pokeapi.co/api/v2/pokemon?limit=150")
        const shuffledArray = data.results.sort(() => 0.5 - Math.random())
        const selectedPokemons = shuffledArray.slice(0, 3)
        return selectedPokemons
@@ -17,9 +17,9 @@ class TeamRepository {
         const findOne = await request(pokemons[0].url)
         const findTwo = await request(pokemons[1].url)
         const findThree = await request(pokemons[2].url)
-        Promise.all([findOne, findTwo, findThree]).then((values) => {
-            console.log(values.name);
-        });
+        const reponse = await Promise.all([findOne, findTwo, findThree])
+
+        return reponse
     }
 }
 
