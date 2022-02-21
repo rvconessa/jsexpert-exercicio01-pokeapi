361aea6dfe15ac3eb6f6754db9452059919cc370
Renan <rvconessa@gmail.com>
2022-02-21 09:21:07 -0300

    feat: add router system

--- src/api.js
+++ src/api.js
@@ -1,1 +1,18 @@
-console.info('🚀 Your API Running right here!');
+const http = require('http');
+const TeamRepository = require('./repository/teamRepository');
+
+const server = http.createServer((req, res) => {
+    const { url } = req
+
+    if(url === "/team") {
+        const teamRepository = new TeamRepository()
+        teamRepository.findRandomPokemons()
+        res.write("team")
+        res.end()
+    } else {
+        res.write("hello world")
+        res.end()
+    }
+})
+
+server.listen(3000)
